553bf004fd914bcc05c99cca8573e6bf388f844e
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-08-12 22:05:29 +0100

    fix divergent branches

--- client/src/components/pages/SignUp.jsx
+++ client/src/components/pages/SignUp.jsx
@@ -26,7 +26,7 @@ const SignUp = () => {
 
         try {
             // Make a POST request to the appropriate API endpoint based on the action
-            const response = await fetch(`http://127.0.0.1:800/register/`, {
+            const response = await fetch(`http://127.0.0.1:8000/register/`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
