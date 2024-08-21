b076c21273f594e980bc8732f132540c9eccd4d0
BlessKobby <amablebless@gmail.com>
2024-08-21 18:29:09 +0000

    Improved on dentist login and addressed some compilation warnings.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,127 +6,125 @@ import { Button } from "./Button";
 import { AuthContext } from "../context/AuthContext";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-    const { isLoggedIn, signupSuccess, logOut } = useContext(AuthContext); // Access signupSuccess from AuthContext
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const { isLoggedIn, signupSuccess, logOut } = useContext(AuthContext);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-    useEffect(() => {
-        showButton();
-    }, []);
-
+  useEffect(() => {
+    showButton();
     window.addEventListener("resize", showButton);
+    return () => window.removeEventListener("resize", showButton);
+  }, []);
 
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        SmileNow <i className="fab fa-typo3" />
-                    </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
-                    </div>
-                    <ul className={click ? "nav-menu active" : "nav-menu"}>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
-                                Services
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/dentists" className="nav-links" onClick={closeMobileMenu}>
-                                Book Now
-                            </Link>
-                        </li>
-                        {/* Mobile-only links */}
-                        {!isLoggedIn && !signupSuccess && (
-                            <li>
-                                <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
-                                    Sign Up
-                                </Link>
-                            </li>
-                        )}
-                        <li>
-                            {isLoggedIn ? (
-                                <div
-                                    className="nav-links-mobile"
-                                    onClick={() => {
-                                        logOut();
-                                        closeMobileMenu();
-                                    }}
-                                >
-                                    Logout
-                                </div>
-                            ) : (
-                                <Link to="/log-in" className="nav-links-mobile" onClick={closeMobileMenu}>
-                                    Log In
-                                </Link>
-                            )}
-                        </li>
-                    </ul>
-                    {button && (
-                        <div className="btn-container">
-                            {/* Hide "Sign Up" button if isLoggedIn or signupSuccess is true */}
-                            {!isLoggedIn && !signupSuccess && (
-                                <div className="btn-wrapper">
-                                    <Link to="/sign-up" className="btn-link">
-                                        <Button 
-                                            route="/sign-up" 
-                                            buttonStyle="btn--outline" 
-                                            buttonSize="btn--large"
-                                        >
-                                            Sign Up
-                                        </Button>
-                                    </Link>
-                                </div>
-                            )}
-                            {isLoggedIn ? (
-                                <div className="btn-wrapper">
-                                    <Link to ="/">
-                                        <Button
-                                            route="/"
-                                            buttonStyle="btn--outline"
-                                            buttonSize="btn--medium"
-                                            onClick={logOut}
-                                        >
-                                            Log Out
-                                        </Button>
-                                        </Link>
-                                </div>
-                            ) : (
-                                <div className="btn-wrapper">
-                                    <Link to="/log-in" className="btn-link">
-                                        <Button 
-                                            route="/log-in" 
-                                            buttonStyle="btn--outline" 
-                                            buttonSize="btn--medium"
-                                        >
-                                            Log In
-                                        </Button>
-                                    </Link>
-                                </div>
-                            )}
-                        </div>
-                    )}
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            SmileNow <i className="fab fa-typo3" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/dentists" className="nav-links" onClick={closeMobileMenu}>
+                Book Now
+              </Link>
+            </li>
+            {!isLoggedIn && !signupSuccess && (
+              <li>
+                <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Sign Up
+                </Link>
+              </li>
+            )}
+            <li>
+              {isLoggedIn ? (
+                <div
+                  className="nav-links-mobile"
+                  onClick={() => {
+                    logOut();
+                    closeMobileMenu();
+                  }}
+                >
+                  Logout
                 </div>
-            </nav>
-        </>
-    );
+              ) : (
+                <Link to="/log-in" className="nav-links-mobile" onClick={closeMobileMenu}>
+                  Log In
+                </Link>
+              )}
+            </li>
+          </ul>
+          {button && (
+            <div className="btn-container">
+              {!isLoggedIn && !signupSuccess && (
+                <div className="btn-wrapper">
+                  <Link to="/sign-up" className="btn-link">
+                    <Button
+                      route="/sign-up"
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--large"
+                    >
+                      Sign Up
+                    </Button>
+                  </Link>
+                </div>
+              )}
+              {isLoggedIn ? (
+                <div className="btn-wrapper">
+                  <Link to="/">
+                    <Button
+                      route="/"
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--medium"
+                      onClick={logOut}
+                    >
+                      Log Out
+                    </Button>
+                  </Link>
+                </div>
+              ) : (
+                <div className="btn-wrapper">
+                  <Link to="/log-in" className="btn-link">
+                    <Button
+                      route="/log-in"
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--medium"
+                    >
+                      Log In
+                    </Button>
+                  </Link>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
--- client/src/components/pages/Dentist_Login.js
+++ client/src/components/pages/Dentist_Login.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import "./dentist_login.css"
+import "./Dentist_login.css"
 import { useNavigate } from "react-router-dom";
 
 const Dentist_Login = () => {
@@ -42,12 +42,12 @@ const Dentist_Login = () => {
 
   return (
     <div className="login-form">
-        <h1>Login</h1>
+        <h1>Log In</h1>
       <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="enter your email" value={email} onChange={(e)=> setEmail(e.target.value)}></input>
-        <input type="password" placeholder="enter your password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+        <input type="email" placeholder="Enter email" value={email} onChange={(e)=> setEmail(e.target.value)}></input>
+        <input type="password" placeholder="Enter password" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
         <p>{error}</p>
-        <button type="submit">Login page for dentist</button>
+        <button type="submit">Login for Dentist</button>
       </form>
     </div>
   );
