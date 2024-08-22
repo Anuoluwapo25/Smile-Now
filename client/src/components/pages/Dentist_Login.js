65ad1edb38faede8a42a58e6d5c8593b1106d8bc
BlessKobby <amablebless@gmail.com>
2024-08-22 00:00:50 +0000

    Fixed compilation errors.

--- client/src/components/pages/Dentist_Login.js
+++ client/src/components/pages/Dentist_Login.js
@@ -34,8 +34,8 @@ const Dentist_Login = () => {
 
             navigate("/dentists-dashboard")            
         } catch (error) {
-            setError(error.message); // Set the error message to display to the user
-            console.error("Error:", error); // Log the error to the console for debugging
+            setError(error.message);
+            console.error("Error:", error);
         }
     };
 
